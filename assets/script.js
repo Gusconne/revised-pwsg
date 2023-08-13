8aa3fb838c76c3dde47674384caf4862a1e03b73
Gusconne <141155419+Gusconne@users.noreply.github.com>
2023-08-13 09:08:39 -0400

    <updates to javascript>

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++) {console.log(topics[x]);}
+
+if (topics === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topics === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topics === 'Git') {
+  console.log("Let's study Git!");
+} else if (topics === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+
+
+
+ 
