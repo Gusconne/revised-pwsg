f9fd726796a85f0ada29dcc95a8a837a209aafc8
Gusconne <141155419+Gusconne@users.noreply.github.com>
2023-08-14 06:43:39 -0400

    updates to js file

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,35 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {console.log(topics[x]);}
+var randomTopic = [Math.floor(Math.random() *  topics.length)];
 
-if (topics === 'HTML') {
+
+function listTopics() {
+for (var x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+}
+
+}
+
+
+function selectTopic() {
+if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topics === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topics === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
+}
 
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
 
 
  
+
+
